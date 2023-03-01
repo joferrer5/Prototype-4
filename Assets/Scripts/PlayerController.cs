c79d81f7f3a387ceaaf04a85c77369b887a4fd08
joferrer5 <joferrer@s.sfusd.edu>
2023-03-01 10:40:16 -0800

    4.1 step 6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     public float speed = 5.0f;
+    private GameObject focalPoint;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
     }
 
     // Update is called once per frame
     void Update()
     {
         float fowardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(Vector3.forward * speed * fowardInput);
+        playerRb.AddForce(focalPoint.transform.forward * speed * fowardInput);
     }
 }
